0ea9654e2b1358aa32152b2357b4ffb4f1376b78
JoeNeu <37147189+JoeNeu@users.noreply.github.com>
2020-12-21 23:58:46 +0100

    add day4 part 2 and refactor ... not proud of day4 :/

--- src/main/kotlin/days/Day4.kt
+++ src/main/kotlin/days/Day4.kt
@@ -1,11 +1,16 @@
 package days
 
+import utils.Person
 import utils.ReadFile
 
 class Day4 {
     private val list = ReadFile.readFileAsLinesUsingReadLines("src/main/resources/day4.txt")
 
-    fun isValid(): Int {
+    private val eyeColor = listOf(
+            "amb", "blu", "brn", "gry", "grn", "hzl", "oth"
+    )
+
+    fun isPassportValid(): Int {
         var count = 0
         var person = Person(false, false, false, false, false, false, false)
         list.forEach {row ->
@@ -29,15 +34,59 @@ class Day4 {
         }
         return count
     }
+
+    fun isPassportValidExtended(): Int {
+        var count = 0
+        var person = Person(false, false, false, false, false, false, false)
+        list.forEach {row ->
+            if(!row.isEmpty()) {
+                row.split(" ").forEach {item ->
+                    val part = item.split(":")
+                    if(part[0].contains("byr")) {
+                        if(part[1].toInt() in 1920..2002)
+                            person.byr = true
+                    }
+                    if(part[0].contains("iyr")) {
+                        if(part[1].toInt() in 2010..2020)
+                            person.iyr = true
+                    }
+                    if(part[0].contains("eyr")) {
+                        if(part[1].toInt() in 2020..2030)
+                            person.eyr = true
+                    }
+                    if(part[0].contains("hgt")) {
+                        if(part[1].contains("cm")) {
+                            if(part[1].filter { it.isDigit() }.toInt() in 150..193)
+                                person.hgt = true
+                        }
+                        if(part[1].contains("in")) {
+                            if(part[1].filter { it.isDigit() }.toInt() in 59..76)
+                                person.hgt = true
+                        }
+                    }
+                    if(part[0].contains("hcl")) {
+                        if(part[1].matches(Regex("#[0-9a-f]{6}")))
+                            person.hcl = true
+                    }
+                    if(part[0].contains("ecl")) {
+                        if(eyeColor.contains(part[1]))
+                            person.ecl = true
+                    }
+                    if(part[0].contains("pid")) {
+                        if(part[1].length == 9)
+                            person.pid = true
+                    }
+                }
+            } else {
+                if (person.byr && person.iyr && person.eyr && person.hgt && person.hcl && person.ecl && person.pid) {
+                    count++
+                }
+                person = Person(false, false, false, false, false, false, false)
+            }
+        }
+        if (person.byr && person.iyr && person.eyr && person.hgt && person.hcl && person.ecl && person.pid) {
+            count++
+        }
+        return count
+    }
 }
-
-data class Person (
-        var byr: Boolean,
-        var iyr: Boolean,
-        var eyr: Boolean,
-        var hgt: Boolean,
-        var hcl: Boolean,
-        var ecl: Boolean,
-        var pid: Boolean
-)
-
---
+++ src/main/kotlin/utils/Person.kt
@@ -0,0 +1,11 @@
+package utils
+
+data class Person (
+        var byr: Boolean,
+        var iyr: Boolean,
+        var eyr: Boolean,
+        var hgt: Boolean,
+        var hcl: Boolean,
+        var ecl: Boolean,
+        var pid: Boolean
+)
