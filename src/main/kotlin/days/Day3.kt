c016a9e1b7267148b921b9a92448f0c972efd7f4
JoeNeu <37147189+JoeNeu@users.noreply.github.com>
2020-12-21 20:48:16 +0100

    add day3 first part

--- src/main/kotlin/days/Day3.kt
+++ src/main/kotlin/days/Day3.kt
@@ -5,8 +5,17 @@ import java.io.File
 class Day3 {
     private val day3 = "src/main/resources/day3.txt"
 
-    fun count() {
-        val list = readFileAsLinesUsingReadLines(day3)
+    fun count(): Int {
+        val list = readFileAsLinesUsingReadLines(day3).map{
+            line -> line.toCharArray()
+        }
+        var count = 0
+        var x = 3
+        list.drop(1).forEach{row ->
+            if(row[x] == '#') count++
+            x = (x+3) % 31
+        }
+        return count
     }
 
     fun readFileAsLinesUsingReadLines(fileName: String): List<String>
