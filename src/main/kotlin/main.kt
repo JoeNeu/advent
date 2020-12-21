1451bf6e634da193d33d3c89225c926102a19f33
JoeNeu <37147189+JoeNeu@users.noreply.github.com>
2020-12-21 19:55:25 +0100

    update main

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,8 +4,8 @@ import days.DayTwo
 fun main(args: Array<String>) {
     val dayOne = DayOne()
     val dayTwo = DayTwo()
-//    println("Day1: " + dayOne.findTwo())
-//    println("Day1: " + dayOne.findThree())
+    println("Day1: " + dayOne.findTwo())
+    println("Day1: " + dayOne.findThree())
+    println("Day2: " + dayTwo.findCorrectPasswords())
     println("Day2: " + dayTwo.findCorrectPasswordsExtended())
-
 }
