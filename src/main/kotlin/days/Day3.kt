0cb2e219dcaa1086ff550554c9c6705d77003e5e
JoeNeu <37147189+JoeNeu@users.noreply.github.com>
2020-12-21 22:25:34 +0100

    add day3 complete and refactor structure

--- src/main/kotlin/days/Day3.kt
+++ src/main/kotlin/days/Day3.kt
@@ -1,28 +1,33 @@
 package days
 
 import utils.ReadFile.readFileAsLinesUsingReadLines
+import java.math.BigInteger
 
 class Day3 {
     private val list = readFileAsLinesUsingReadLines("src/main/resources/day3.txt").map{
         line -> line.toCharArray()
     }
 
-    fun countTrees(): Int {
-        var count = 0
-        var x = 3
-        list.drop(1).forEach{row ->
-            if(row[x] == '#') count++
-            x = (x+3) % 31
-        }
-        return count
+    fun countTrees(): BigInteger {
+        return search(3)
     }
 
-    fun countTreesExtended(): Int {
-        var count = 0
-        var x = 3
+    fun countTreesExtended(): BigInteger {
+        return search(1) * search(3) * search(5) * search(7) * search(1, true)
+    }
+
+    fun search(shift: Int, twoSteps: Boolean = false): BigInteger {
+        var x = shift
+        var jumpRow = twoSteps
+        var count = BigInteger.ZERO
         list.drop(1).forEach{row ->
-            if(row[x] == '#') count++
-            x = (x+3) % 31
+            if(jumpRow) {
+                jumpRow = false
+            } else {
+                if(row[x] == '#') count++
+                x = (x+shift) % 31
+                if(twoSteps) jumpRow = true
+            }
         }
         return count
     }
